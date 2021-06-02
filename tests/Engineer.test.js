5c3ee5e8026e0e29c764aafd6bf53df990acbed7
Donna <donnastjames@comcast.net>
2021-06-01 22:17:05 -0700

    Added Engineer.test

---
+++ tests/Engineer.test.js
@@ -0,0 +1,29 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  it("can be constructed with a name, id, email and github property", () => {
+    const engineer = new Engineer("  Laura Ingalls  ", "  2234  ", "  laura.ingalls@email.com   ", "  github.com/lauraingalls ");
+
+    // Verify that the new Employee has the correct properties
+    expect(engineer.name).toEqual("Laura Ingalls");
+    expect(engineer.id).toEqual("2234");
+    expect(engineer.email).toEqual("laura.ingalls@email.com");
+    expect(engineer.github).toEqual("github.com/lauraingalls");
+  });
+
+  it("should throw an error for invalid github parameter", () => {
+    const cb = () => new Engineer("Jane Doe", "246-9", "jane.doe@gmail.com", null);
+    const err = new Error("Cannot read property 'trim' of null");
+    expect(cb).toThrowError(err);
+  });
+
+  it("getGithub() returns expected Github profile", () => {
+    const engineer = new Engineer("Laura Ingalls", "2234", "laura.ingalls@email.com", "github.com/lauraingalls");
+    expect(engineer.getGithub()).toEqual("github.com/lauraingalls");
+  });
+
+  it("getRole() returns the string Engineer", () => {
+    const engineer = new Engineer("Laura Ingalls", "2234", "laura.ingalls@email.com", "github.com/lauraingalls", "Engineer");
+    expect(engineer.getRole()).toEqual("Engineer");
+  });
+});
